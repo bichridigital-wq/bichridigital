c9af14f0f2096c4862a57430e676f5ffb1339fe1
BOUNAMA NIANG <bichridigital@gmail.com>
2026-06-27 02:37:53 +0000

    Ajout navbar et footer page contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,25 @@
-
+import Navbar from "../navbar";
 import ContactHero from "../components/contact/ContactHero";
 import ContactCards from "../components/contact/ContactCards";
 import ContactForm from "../components/contact/ContactForm";
 import ContactMap from "../components/contact/ContactMap";
 import ContactCTA from "../components/contact/ContactCTA";
+import ContactFooter from "../components/contact/contactfooter";
 
 export default function ContactPage() {
   return (
-    <main className="bg-[#020B2E] text-white">
-      <ContactHero />
-      <ContactCards />
-      <ContactForm />
-      <ContactMap />
-      <ContactCTA />
-    </main>
+    <>
+      <Navbar />
+
+      <main className="bg-[#020B2E] text-white pt-24">
+        <ContactHero />
+        <ContactCards />
+        <ContactForm />
+        <ContactMap />
+        <ContactCTA />
+      </main>
+
+      <ContactFooter />
+    </>
   );
 }
