423d276bbe9b49cb461f936c74392a82ec9414c3
BOUNAMA NIANG <bichridigital@gmail.com>
2026-06-29 16:07:04 +0000

    Creation page portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 const categories = [
   "Tous",
