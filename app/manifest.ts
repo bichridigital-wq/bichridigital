2a001ff2fb23e296b5e204ddfd142f71dbeaa42a
BOUNAMA NIANG <bichridigital@gmail.com>
2026-07-09 13:29:29 +0000

    Ajout icones PWA Bichridigital

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,24 +15,24 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ["business", "photo", "productivity"],
     lang: "fr",
     icons: [
-      {
-        src: "/logo.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/logo.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/logo.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-    ],
+  {
+    src: "/icons/icon-192.png",
+    sizes: "192x192",
+    type: "image/png",
+    purpose: "any",
+  },
+  {
+    src: "/icons/icon-512.png",
+    sizes: "512x512",
+    type: "image/png",
+    purpose: "any",
+  },
+  {
+    src: "/icons/maskable-512.png",
+    sizes: "512x512",
+    type: "image/png",
+    purpose: "maskable",
+  },
+],
   };
 }
