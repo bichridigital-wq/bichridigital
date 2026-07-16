40ca1f5173aa1221c6cfb2d11f4f63c74ee2db6d
BOUNAMA NIANG <bichridigital@gmail.com>
2026-07-16 01:08:07 +0000

    Correction du sitemap avec www

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://bichridigital.com";
+  const baseUrl = "https://www.bichridigital.com";
 
   const routes = [
     "",
